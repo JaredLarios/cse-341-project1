e63d117428677aadd27b366c26c81d288eb78c4b
JaredLarios <lar21038@byui.edu>
2025-05-13 20:02:16 -0600

    Implement CRUD operations for contacts and add body-parser middleware

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,23 +2,69 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection("users").find();
-    result.toArray().then((users) => {
+    const response = await mongodb.getDatabase().db().collection("contacts").find();
+    response.toArray().then((contacts) => {
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json(users);
+        res.status(200).json(contacts);
     })
 };
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection("users").find({_id: userId});
-    result.toArray().then((users) => {
+    const response = await mongodb.getDatabase().db().collection("contacts").find({_id: userId});
+    response.toArray().then((contacts) => {
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json(users[0]);
+        res.status(200).json(contacts[0]);
     })
+};
+
+const createContact = async (req, res) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+    const response = await mongodb.getDatabase().db().collection("contacts").insertOne(contact);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "Some error occurred while update the contact.")
+    }
+};
+
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+    const response = await mongodb.getDatabase().db().collection("contacts").replaceOne({_id: userId}, contact);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "Some error occurred while update the contact.")
+    }
+};
+
+const deleteContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection("contacts").deleteOne({_id: userId});
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "Some error occurred while update the contact.")
+    }
 };
 
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
+const bodyParser = require('body-parser');
 const router = require('express').Router();
 
 router.get("/", (req, res) => res.send("Hello World"));
 
+router.use(bodyParser.json());
 router.use("/contacts", require('./contacts'));
 
 module.exports = router;
